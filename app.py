4b5d228b56d3c4ef64cbeec0926ce3c3bd15250f
k <kevin.parkkreutz@gmail.com>
2020-10-03 22:08:10 +0200

    simple index added

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import *
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route('/<name>')
+def hello_world(name):
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
